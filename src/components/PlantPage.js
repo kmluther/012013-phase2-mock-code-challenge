b7f30e4a571dad9ae4d6ba0078a277a4ccca0832
Katelyn Luther <katelynluther87@aim.com>
2023-03-01 11:08:46 -0700

    core deliverables met

--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -1,14 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
+const plantAPI = "http://localhost:6001/plants";
+
 function PlantPage() {
+  const [plants, setPlants] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+
+  useEffect(() => {
+  fetch(plantAPI)
+  .then(res => res.json())
+  .then(setPlants);
+  }, []);
+
+  const addNewPlant = (myNewPlant) => {
+    setPlants([...plants, myNewPlant])
+  }
+
+  const updateSearch = (searchInput) => {
+    setSearchTerm(searchInput)
+  }
+
+  let filteredPlants = plants.filter(plant => plant.name.toLowerCase().includes(searchTerm.toLowerCase()))
+
   return (
     <main>
-      <NewPlantForm />
-      <Search />
-      <PlantList />
+      <NewPlantForm addNewPlant={addNewPlant}/>
+      <Search searchTerm={searchTerm} updateSearch={updateSearch}/>
+      <PlantList plants={filteredPlants}/>
     </main>
   );
 }
